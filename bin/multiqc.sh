bf9d976af2768707d7d74ae84291781ada96b324
Yongchao Ge <yongchao.ge@gmail.com>
2019-04-14 15:50:31 -0400

    multqc regresses to 1.6

--- bin/multiqc.sh
+++ bin/multiqc.sh
@@ -11,10 +11,10 @@ export LANG=en_US.UTF-8
 export TMPDIR=tmpdir
 mkdir -p $TMPDIR
 
-set +e #multiqc gives warning
+#to do for the bug in cluster
 multiqc -d \
 	-f \
 	-n $name \
 	-o multiqc \
 	"$@"
-set -e
+
