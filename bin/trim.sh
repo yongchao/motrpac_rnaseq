0eddf82dd39030fc525d9ccb094225d30016db88
Yongchao Ge <yongchao.ge@gmail.com>
2019-05-03 16:23:44 -0400

    fix the bug for single trim

--- bin/trim.sh
+++ bin/trim.sh
@@ -32,18 +32,19 @@ shift $((OPTIND-1))
 #$2, when present, must be the same as $1 with R1 replaced by R2, not enforced here for now
 R1=$(basename $1)
 R2=""
+index2=""
 cmdo2=""
 cmd_tooshort2=""
 
 if (( $# == 2 )); then
     R2=$(basename $2)
+    index2="-A $univ_adapter"
     cmdo2="-p fastq_trim/$R2"
     cmd_tooshort2="--too-short-paired-output fastq_trim/tooshort/$R2"
 fi
 mkdir -p fastq_trim/tooshort    
 cutadapt \
-    -a $index_adapter \
-    -A $univ_adapter \
+    -a $index_adapter $index2 \
     -o fastq_trim/$R1 $cmdo2\
     -m 20 \
     --too-short-output fastq_trim/tooshort/$R1 $cmd_tooshort2\
