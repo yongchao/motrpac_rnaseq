2e76b301c2b3bd9458ab40898c01befc912db962
Yongchao Ge <yongchao.ge@gmail.com>
2020-04-23 19:37:11 -0400

    fix the featureCounts problem

--- bin/featureCounts.sh
+++ bin/featureCounts.sh
@@ -11,7 +11,23 @@ if (( $paired == 1 )); then
     pairopt="-p"
 fi
 
-tmpdir=$(mktemp -d -p $tmpdir_root featureCounts.${SID}.XXX)  
+tmpdir=$(mktemp -d -p $tmpdir_root featureCounts.${SID}.XXX)
 
-featureCounts -T $threads --tmpDir $tmpdir  -a $gdir/genome.gtf -o featureCounts/$SID $pairopt -M --fraction $bam
+sam=$tmpdir/$SID.sam
+sam_sorted=$tmpdir/${SID}_sorted.sam
+sam_full=$tmpdir/${SID}_full.sam
+		     
+#samtools sort -m 5.5G -n -T $tmpdir/samsort -@ $threads -o $bam_paired $bam
+#it generates too many small files and it is not working well
+
+samtools view -@ $threads $bam >$sam
+
+export LC_ALL=C; export LC_LANG=C
+sort $sam -k1,1 -o $sam_sorted -T $tmpdir --parallel=$threads -S 5G
+samtools view -H $bam >$sam_full
+cat $sam_sorted >>$sam_full
+
+#this seems promising
+
+featureCounts -T $threads --tmpDir $tmpdir  -a $gdir/genome.gtf -o featureCounts/$SID $pairopt -M --fraction $sam_full --donotsort
 rm -rf $tmpdir
